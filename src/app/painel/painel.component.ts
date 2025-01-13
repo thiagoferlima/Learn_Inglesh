1bbc4e5039f9f6edcc74d1e7c748111876212fc2
Thiagoferlima <th1gox2003@gmail.com>
2025-01-13 15:58:24 -0300

    check answer

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,14 +12,17 @@ export class PainelComponent implements OnInit {
 
   public instrucao: string = 'Traduza a frase:'
 
-
+  public resposta: string
   constructor() { console.log(this.frases)}
 
   ngOnInit() {
   }
-  public atualizarResposta(): void{
-
-    console.log('test')
+  public atualizarResposta(resposta: Event): void{
+    this.resposta =((<HTMLInputElement>resposta.target).value)
+    //console.log(this.resposta)
+  }
+  public verificarResposta():void {
+    console.log('Verifiar resposta: ', this.resposta)
 
   }
 
