b22906f3db8f14023e02363c86dc64c8c0fe8aa1
Thiagoferlima <th1gox2003@gmail.com>
2025-01-13 16:40:33 -0300

    check answer2

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -9,21 +9,42 @@ import { FRASES } from './frases-mock';
 export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
-
   public instrucao: string = 'Traduza a frase:'
-
   public resposta: string
-  constructor() { console.log(this.frases)}
+  public rodada: number =0
+  public rodadaFrase: Frase
+
+
+  constructor() 
+  { 
+    this.rodadaFrase = this.frases[this.rodada],
+    console.log(this.rodadaFrase)
+
+  }
 
   ngOnInit() {
   }
   public atualizarResposta(resposta: Event): void{
     this.resposta =((<HTMLInputElement>resposta.target).value)
-    //console.log(this.resposta)
+    
   }
   public verificarResposta():void {
-    console.log('Verifiar resposta: ', this.resposta)
 
+    if(this.rodadaFrase.frasePtbr == this.resposta){
+      alert('A tradução está correta')
+
+      this.rodada++
+
+      
+      this.rodadaFrase = this.frases[this.rodada]
+      
+    }
+    else{
+      alert('A tradução está errado')
+    }
+  
+    
+ 
   }
 
 }
