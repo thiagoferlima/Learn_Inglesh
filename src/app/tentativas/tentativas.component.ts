28ff07dcba60b7cb4daba7d20a941ed8a400eaf1
Thiagoferlima <th1gox2003@gmail.com>
2025-01-16 10:01:26 -0300

    ControllingAttempts

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -19,6 +19,12 @@ export class TentativasComponent implements OnInit,OnChanges {
     console.log(this.coracoes)
   }
   ngOnChanges()  {
+    if(this.tentativas !==  this.coracoes.length){
+      let indice = this.coracoes.length - this.tentativas
+      this.coracoes[indice -1].cheio = false
+
+
+    }
     console.log('Tentativas recebidas do painel: ', this.tentativas)
 
   }
